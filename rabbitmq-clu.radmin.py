46529e44e08c30592df1c4a44a49063682ec0f2d
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2015-03-24 09:27:59 +0100

    [ACC-64] remove sed (not working on docker /etc/hosts)

--- rabbitmq-clu.radmin.py
+++ rabbitmq-clu.radmin.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from os import abort
+import os
+import shutil
 import socket
 import subprocess
 
@@ -41,12 +43,14 @@ def setup_dns():
     if cluster_nodes_definition is None:
         abort(400)
     else:
+        if not os.path.isfile("/etc/hosts.orig"):
+            shutil.copyfile("/etc/hosts", "/etc/hosts.orig")
+
+        shutil.copyfile("/etc/hosts.orig", "/etc/hosts")
+
         for node_definition in cluster_nodes_definition:
             node_name = node_definition.get('nodeName')
             node_fqdn = node_definition.get('nodeFQDN')
-
-            clean_host_if_needed_cmd = "rm=`more /etc/hosts | grep " + node_name + "` ; sed -i \"s#$rm##g\" /etc/hosts"
-            ret = subprocess.call(clean_host_if_needed_cmd, shell=True)
 
             ip = None
             ais = socket.getaddrinfo(node_fqdn, 0, 0, 0, 0)
@@ -62,21 +66,9 @@ def setup_dns():
 
 @app.route('/api/0.1/cleanHosts', methods=['POST'])
 def clean_dns():
-    if not request.json:
-        abort(400)
-
-    cluster_nodes_definition = request.json
-    print cluster_nodes_definition
-    if cluster_nodes_definition is None:
-        abort(400)
-    else:
-        for node_definition in cluster_nodes_definition:
-            node_name = node_definition.get('nodeName')
-
-            clean_host_if_needed = "rm=`more /etc/hosts | grep " + node_name + "` ; sed -i \"s#$rm##g\" /etc/hosts"
-            ret = subprocess.call(clean_host_if_needed, shell=True)
-
-        return jsonify(result={"status": 200})
+    if os.path.isfile("/etc/hosts.orig"):
+        shutil.copyfile("/etc/hosts.orig", "/etc/hosts")
+    return jsonify(result={"status": 200})
 
 @app.route('/api/0.1/connect', methods=['POST'])
 def connect_cluster():
@@ -84,30 +76,16 @@ def connect_cluster():
         abort(400)
 
     cluster_name = str(request.json.get('clusterName'))
-    primary_node_fqdn = str(request.json.get('primaryNodeFQDN'))
 
-    if cluster_name is None or primary_node_fqdn is None:
+    if cluster_name is None:
         abort(400)
     else:
-        clean_host_if_needed_cmd = "rm=`more /etc/hosts | grep " + cluster_name + "` ; sed -i \"s#$rm##g\" /etc/hosts"
-        ret = subprocess.call(clean_host_if_needed_cmd, shell=True)
-
-        ip = None
-        ais = socket.getaddrinfo(primary_node_fqdn, 0, 0, 0, 0)
-        for result in ais:
-            ip = str(result[-1][0])
-            break
-
-        host_entry = ip + "    " + cluster_name
-        add_host_entry_cmd = "echo \"" + host_entry + "\" >> /etc/hosts"
-        ret = subprocess.call(add_host_entry_cmd, shell=True)
-
         ret = subprocess.call("rabbitmqctl stop_app", shell=True)
         join_cluster_cmd = "rabbitmqctl join_cluster rabbit@" + cluster_name
         ret = subprocess.call(join_cluster_cmd, shell=True)
         ret = subprocess.call("rabbitmqctl start_app", shell=True)
 
-        return jsonify(result={"status": 200, "info": 'Connected To Cluster ' + cluster_name + ' (' + primary_node_fqdn + ') !'})
+        return jsonify(result={"status": 200, "info": 'Connected To Cluster ' + cluster_name + '!'})
 
 @app.route('/api/0.1/disconnect', methods=['POST'])
 def disconnect_cluster():
@@ -119,8 +97,6 @@ def disconnect_cluster():
     if cluster_name is None:
         abort(400)
     else:
-        clean_host_entry_cmd = "rm=`more /etc/hosts | grep " + cluster_name + "` ; sed -i \"s#$rm##g\" /etc/hosts"
-        ret = subprocess.call(clean_host_entry_cmd, shell=True)
         ret = subprocess.call("rabbitmqctl stop_app", shell=True)
         ret = subprocess.call("rabbitmqctl reset", shell=True)
         ret = subprocess.call("rabbitmqctl start_app", shell=True)
